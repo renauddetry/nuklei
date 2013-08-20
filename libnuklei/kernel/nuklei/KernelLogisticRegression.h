f9dadd4e5dc3a2ab8b1256d9d21f19f1d54b2ec4
Renaud Detry <renaud.detry@ulg.ac.be>
2013-08-20 16:18:33 +0200

    Renamed classes ending in ...ion to ..or

--- libnuklei/kernel/nuklei/KernelLogisticRegression.h
+++ libnuklei/kernel/nuklei/KernelLogisticRegression.h
@@ -8,116 +8,28 @@
 #ifndef NUKLEI_KERNEL_LOGISTIC_REGRESSION_H
 #define NUKLEI_KERNEL_LOGISTIC_REGRESSION_H
 
+#include <nuklei/KernelLogisticRegressor.h>
 
-#include <nuklei/KernelCollection.h>
+#warning The KernelLogisticRegression class is obsolete. \
+ Use KernelLogisticRegressor instead.
 
 namespace nuklei
 {
-  
   /**
-   * @ingroup kernels
-   * @brief Implements kernel logistic regression.
-   *
-   * This class implements a two-class nonlinear classifier for @f$
-   * SE(3) @f$ data. KLR is discussed in @ref kernels_klr.
-   *
-   * This class is based on @c libklr, which is provided with Makoto Yamada's <a
-   * href="http://sugiyama-www.cs.titech.ac.jp/~yamada/iwklr.html">IWKLR</a>.
+   * Obsolete. See KernelLogisticRegressor instead.
    */
-  struct KernelLogisticRegression
+  struct KernelLogisticRegression : public KernelLogisticRegressor
   {
-    /** @brief */
-    KernelLogisticRegression() {}
-    /**
-     * @brief Imports input from @p data and @p labels, and computes the Gram
-     * matrix of the data.
-     *
-     * The two structures @p data and @p labels should contain the same number
-     * of elements. The @f$ i^{\mathrm{th}} @f$ element of @p labels indicates
-     * the class of the @f$ i^{\mathrm{th}} @f$ element of @p data. The two
-     * allowed values in @p labels are @c 1 and @c 2.
-     */
+    KernelLogisticRegression() : KernelLogisticRegressor() {}
+
     KernelLogisticRegression(const KernelCollection &data,
-                             const std::vector<int>& labels);    
-    /**
-     * @brief Imports input from @p data and @p labels and the Gram matrix @p
-     * gramMatrix.
-     *
-     * The two structures @p data and @p labels should contain the same number
-     * of elements. The @f$ i^{\mathrm{th}} @f$ element of @p labels indicates
-     * the class of the @f$ i^{\mathrm{th}} @f$ element of @p data. The two
-     * allowed values in @p labels are @c 1 and @c 2.
-     *
-     * The data held in @p gramMatrix should correspond to the Gram matrix of @p
-     * data.
-     */
+                             const std::vector<int>& labels) :
+    KernelLogisticRegressor(data, labels) {};
+
     KernelLogisticRegression(const KernelCollection &data,
                              const GMatrix& gramMatrix,
-                             const std::vector<int>& labels);    
-    /**
-     * @brief Imports input from @p data and @p labels, and computes the Gram
-     * matrix of the data.
-     *
-     * The two structures @p data and @p labels should contain the same number
-     * of elements. The @f$ i^{\mathrm{th}} @f$ element of @p labels indicates
-     * the class of the @f$ i^{\mathrm{th}} @f$ element of @p data. The two
-     * allowed values in @p labels are @c 1 and @c 2.
-     *
-     * This method also resets all the other member variables of the class.
-     */
-    void setData(const KernelCollection &data,
-                 const std::vector<int>& labels);
-    
-    /**
-     * @brief Computes KLR weights.
-     *
-     * The implementation follows <a
-     * href="http://dx.doi.org/10.1016/j.sigpro.2009.06.001">Semi-supervised
-     * speaker identification under covariate shift</a>.
-     */
-    void train(const double delta = 0.0001, const unsigned itrNewton = 5);
-    
-    /**
-     * @brief Returns @c true if the classifier has been trained.
-     */
-    bool isTrained()
-    {
-      return vklr_;
-    };
-    
-    /**
-     * @brief Returns a pair of values which indicate the probability of classes
-     * @c 1 and @c 2.
-     */
-    Vector2 test(const kernel::base &t) const;
-    /**
-     * @brief Returns the probabilities of classes @c 1 and @c 2 for all data
-     * points in @p testSet.
-     *
-     * This method returns a @f$ 2 \times n @f$ matrix, where @f$ n @f$ is the
-     * number of elements in @p testSet.
-     */
-    GMatrix test(const KernelCollection &testSet) const;
-        
-    /**
-     * @brief Returns KLR weights.
-     *
-     * The parameter @p delta is the regularization constant.
-     */
-    const GMatrix& vklr() const
-    {
-      NUKLEI_TRACE_BEGIN();
-      NUKLEI_ASSERT(vklr_);
-      return *vklr_;
-      NUKLEI_TRACE_END();
-    }
-  private:
-    void computeGramMatrix();
-    
-    KernelCollection trainSet_;
-    GMatrix gramMatrix_;
-    std::vector<int> labels_;
-    boost::optional<GMatrix> vklr_;
+                             const std::vector<int>& labels) :
+    KernelLogisticRegressor(data, gramMatrix, labels) {};
   };
 }
 
