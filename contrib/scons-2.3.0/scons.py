aeeca39098f36206bbc960535ba4d98d1707fa83
Renaud Detry <renaud.detry@ulg.ac.be>
2013-08-23 18:46:09 +0200

    Changed SCons python warning

--- contrib/scons-2.3.0/scons.py
+++ contrib/scons-2.3.0/scons.py
@@ -57,7 +57,7 @@ import sys
 
 if sys.version_info >= (3,0,0):
     msg = "\nscons: *** SCons version %s does not run under Python version %s.\n\
-Python 3 is not yet supported.\nTo proceed, run SCons with Python 2.7. Typing \n\n$ python2.7 ./scons.py install\n\nis likely to do the trick. If it does not work, install Python 2.7 and figure out how its command name.\n"
+Python 3 is not yet supported.\nTo proceed, run SCons with Python 2.7. Typing \n\n$ python2.7 ./scons.py install\n\nis likely to do the trick. If it does not work, install Python 2.7, and see how it is called (python2.7, python-2.7, python27, ...).\n"
     sys.stderr.write(msg % (__version__, sys.version.split()[0]))
     sys.exit(1)
 
